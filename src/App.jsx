dd29eb4b50026519d7dbba2082c7519c08e528f1
Aditya Kalambe <adityakalambe20@gmail.com>
2024-10-04 22:02:26 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+ import React from 'react';
+// import Navbar from './navbar'
+
+
+
+// eslint-disable-next-line no-undef
+const reactElement = React.createElement(
+  'a',
+  {href: "https://google.com"},
+  'click here to visit google'
+);
+function App() {
+  return (
+      // <>
+      // <Navbar/>
+      //   <h1>Hello Adu</h1>
+      // </>
+      reactElement
+  )
+};
+
+export default App
---
+++ src/navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <nav>
+            <h1>Kashmiri Fashion</h1>
+        </nav>
+    )
+
+}
+
+export default Navbar
